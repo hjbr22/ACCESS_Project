3fe7af21d24d50132d1e10f29413cb3a1772eeca
Sandesh Lamichhane <Sandesh.lamichhane1@gmail.com>
2023-05-26 15:15:18 -0400

    Delete answers.py

--- logic/answers.py
+++
@@ -1,6 +0,0 @@
-from models.rps import RPS
-from models.jobClass import JobClass
-from models.rpJobClass import RpJobClass
-from models.researchField import ResearchFields
-from models.rpResearchField import RpResearchField
-
