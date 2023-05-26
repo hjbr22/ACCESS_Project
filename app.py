e1dace5137ce397b8a707f786498843515c96d03
Sandesh-l <sandesh.lamichhane1@gmail.com>
2023-05-26 13:36:14 -0400

    working on fetching things from db and adding them to the view.

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
+from models.rps import RPS
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return render_template("questions.html")
+
+    rps = RPS.select()
+    print(rps)
+    for rp in rps:
+        print(rp.name)
+    return render_template("questions.html", rps = rps)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
---
+++ logic/answers.py
@@ -0,0 +1,6 @@
+from models.rps import RPS
+from models.jobClass import JobClass
+from models.rpJobClass import RpJobClass
+from models.researchField import ResearchFields
+from models.rpResearchField import RpResearchField
+
