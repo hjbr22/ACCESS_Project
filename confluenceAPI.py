e654bedafd7f57b46318adc792cc093794a59da4
Sandesh-l <sandesh.lamichhane1@gmail.com>
2023-07-19 11:56:46 -0400

    stuff

--- confluenceAPI.py
+++ confluenceAPI.py
@@ -3,6 +3,7 @@ from requests.auth import HTTPBasicAuth
 import json
 import os
 from dotenv import load_dotenv
+from bs4 import BeautifulSoup as bs
 
 def load_api():
    load_dotenv()
@@ -10,7 +11,9 @@ def load_api():
 load_api()
 # The URL endpoint
 url = "https://access-ci.atlassian.net/reset/api/space?spaceKey=ACCESSdocumentation"
-v2url = 'https://access-ci.atlassian.net/wiki/api/v2/pages/278069249?expand=body.storage'
+   # https://access-ci.atlassian.net/wiki/rest/api/content/255754279?expand=body.storage
+v2url = 'https://access-ci.atlassian.net/wiki/rest/api/content/255754279?expand=body.view'
+       # https://access-ci.atlassian.net/wiki/api/v2/pages/278069249?expand=body.storage
 params = {'limit':"250",'expand': 'body.storage'}
 
 auth = HTTPBasicAuth(os.getenv("atlassian_username"),os.getenv("confluence_token"))
@@ -25,5 +28,9 @@ if response.status_code != 200:
    print('Status:', response.status_code, 'Problem with the request. Exiting.')
    exit()
 
-print(response.text)
-print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+# print(response.text)
+# d = json.loads(response.text)
+data = json.loads(response.text)
+body = bs(data['body'])
+print(body.prettify())
+# print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
