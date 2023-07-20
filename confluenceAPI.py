66a824e8d2272006309978a72ca8ebc9ffc3a323
Sandesh-l <sandesh.lamichhane1@gmail.com>
2023-07-20 13:54:15 -0400

    api is now working! updated the README file as well

--- confluenceAPI.py
+++ confluenceAPI.py
@@ -10,27 +10,24 @@ def load_api():
 
 load_api()
 # The URL endpoint
-url = "https://access-ci.atlassian.net/reset/api/space?spaceKey=ACCESSdocumentation"
-   # https://access-ci.atlassian.net/wiki/rest/api/content/255754279?expand=body.storage
-v2url = 'https://access-ci.atlassian.net/wiki/rest/api/content/255754279?expand=body.view'
-       # https://access-ci.atlassian.net/wiki/api/v2/pages/278069249?expand=body.storage
-params = {'limit':"250",'expand': 'body.storage'}
+pageid = '255754279'
+url = f'https://access-ci.atlassian.net/wiki/rest/api/content/{pageid}'
+params = {'expand': 'body.view'}
 
 auth = HTTPBasicAuth(os.getenv("atlassian_username"),os.getenv("confluence_token"))
 headers = {
    'Accept': "application/json"
 }
-
-response = requests.request("GET",v2url,headers=headers,auth=auth)
+response = requests.request("GET",url,headers=headers,auth=auth,params=params)
 
 # Check for HTTP codes other than 200
 if response.status_code != 200:
    print('Status:', response.status_code, 'Problem with the request. Exiting.')
    exit()
 
-# print(response.text)
-# d = json.loads(response.text)
 data = json.loads(response.text)
-body = bs(data['body'])
-print(body.prettify())
-# print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+pageContent = bs(data['body']['view']['value'])
+print(pageContent.prettify())
+with open('confluencePage.html', 'w') as cp:
+   print(type(pageContent.prettify()))
+   cp.write(pageContent.prettify())
