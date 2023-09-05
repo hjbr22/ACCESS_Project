16135309d8fa9bb86949aab8b34c7532c675f9c0
Sandesh-l <sandesh.lamichhane1@gmail.com>
2023-09-05 13:16:26 -0400

    connecting validation

--- confluence/APIValidation.py
+++ confluence/APIValidation.py
@@ -1,37 +1,34 @@
 import pandas as pd
 
 
-
 def validate_numeric_values(column):
     return pd.to_numeric(column, errors='coerce').notna().all()
 
-def validate_table_1(table):
-    # Validate values in Temp Storage column
-   if not validate_numeric_values(table['Temp Storage (TB)']):
+def validate_storage_table(table):
+   # Validate values in Temp Storage column
+   scratch_tb = table.iloc[0,0]
+   longterm_tb = table.iloc[0,1]
+   if not validate_numeric_values(scratch_tb):
       return False, "Temp Storage (TB) column contains non-numeric values"
-   elif not validate_numeric_values(table['Long-Term Storage (TB)']):
+   elif not validate_numeric_values(longterm_tb):
       return False, "Long-Term Storage (TB) column contains non-numeric values"
    return True
 
-def validate_table_2(table):
+def validate_memory_table(table):
    if not validate_numeric_values(table['Amount (GB)']):
-      return False, "Memory (RAM) Node column contains non-numeric values"
+      return False, "Memory (RAM) table contains non-numeric values"
    return True
 
-def validate_table_3(table):
+def validate_gui_table(table):
     # Validate values in Suitability column
    if not table['Suitability'].isin([0, 1, 2]).all():
       return False, "Suitability column does not contain 0, 1, 2"
-    
-    
    return True
 
-def validate_table_4(table):
+def validate_research_field_table(table):
     # Validate values in Suitability column
    if not table['Suitability'].isin([0, 1, 2]).all():
       return False, "Suitability column does not contain 0, 1, 2"
-    
-    
    return True
 
 def validate_table_5(table):
@@ -39,7 +36,7 @@ def validate_table_5(table):
       return False, "Suitability column does not contain 0, 1, 2"
    return True
 
-def validate_table_6(table):
+def validate_job_class_table(table):
     # Validate values in Suitability column
     if not table['Suitability'].isin([0, 1, 2]).all():
       return False, "Suitability column does not contain 0, 1, 2"
