854e8ead59da8cf26b29156aae6418fa6daf8fdc
Sandesh-l <sandesh.lamichhane1@gmail.com>
2023-09-12 13:52:53 -0400

    validation works now

--- confluence/APIValidation.py
+++ confluence/APIValidation.py
@@ -1,44 +1,30 @@
 import pandas as pd
-
+from pandas.api.types import is_numeric_dtype
 
 def validate_numeric_values(column):
-    return pd.to_numeric(column, errors='coerce').notna().all()
+   if len(column) == 0:
+      return True
+   return is_numeric_dtype(column)
 
 def validate_storage_table(table):
    # Validate values in Temp Storage column
-   scratch_tb = table.iloc[0,0]
-   longterm_tb = table.iloc[0,1]
+   columns = table.columns
+   scratch_tb = table[columns[0]]
+   longterm_tb = table[columns[1]]
    if not validate_numeric_values(scratch_tb):
       return False, "Temp Storage (TB) column contains non-numeric values"
    elif not validate_numeric_values(longterm_tb):
       return False, "Long-Term Storage (TB) column contains non-numeric values"
-   return True
+   return True, "Storage table is valid"
 
 def validate_memory_table(table):
    if not validate_numeric_values(table['Amount (GB)']):
       return False, "Memory (RAM) table contains non-numeric values"
-   return True
+   return True, "Memory is valid"
 
-def validate_gui_table(table):
-    # Validate values in Suitability column
-   if not table['Suitability'].isin([0, 1, 2]).all():
+def validate_suitability(table):
+   # Validate values in Suitability column
+   suitibalityColumn = table.columns[1]
+   if not table[suitibalityColumn].isin([0, 1, 2]).all():
       return False, "Suitability column does not contain 0, 1, 2"
-   return True
-
-def validate_research_field_table(table):
-    # Validate values in Suitability column
-   if not table['Suitability'].isin([0, 1, 2]).all():
-      return False, "Suitability column does not contain 0, 1, 2"
-   return True
-
-def validate_table_5(table):
-   if not table['Suitability'].isin([0, 1, 2]).all():
-      return False, "Suitability column does not contain 0, 1, 2"
-   return True
-
-def validate_job_class_table(table):
-    # Validate values in Suitability column
-    if not table['Suitability'].isin([0, 1, 2]).all():
-      return False, "Suitability column does not contain 0, 1, 2"
-
-    return True
+   return True, "suitability is valid"
--- confluence/confluenceAPI.py
+++ confluence/confluenceAPI.py
@@ -32,7 +32,7 @@ def get_page_children_ids(conf,pageID):
         childPageIds.append(page['id'])
     return(childPageIds)
 
-       # # pageJsonList=[]
+      # # pageJsonList=[]
     # # for page_id in childPageIds:
     # #     page = conf.get_page_by_id(page_id)
     # #     print(page)
@@ -47,47 +47,5 @@ def get_tabulated_page_data(conf, pageID):
    table = pd.read_html(pageContent)
    
    return table, pageTitle
-
-# def test_validation(conf, pageID):
-#    conf = get_conf()
-#    table = get_tabulated_page_data(conf, pageID)
-#    print(type(table))
-#    print('\n Number of tables:', len(table))
-#    #get the first table with index 0
-#    First_table = table[0]
-
-#    #get the second table with index 1
-#    Second_table = table[1]
-   
-#    #get the third table with index 2
-#    Third_table = table[2]
-   
-#    #get the fourth table with index 3
-#    Fourth_table = table[3]
-   
-#    #get the fifth table with index 4
-#    Fifth_table = table[4]
-   
-#    #get the sixth table with index 5
-#    Sixth_table = table[5]
-
-#    print('\n Table 1\n')
-#    print(validate_table_1(First_table))
-#    print(First_table)
-#    print('\n Table 2\n')
-#    print(validate_table_2(Second_table))
-#    print(Second_table)
-#    print('\n Table 3\n')
-#    print(validate_table_3(Third_table))
-#    print(Third_table)
-#    print('\n Table 4\n')
-#    print(validate_table_4(Fourth_table))
-#    print(Fourth_table)
-#    print('\n Table 5\n')
-#    print(validate_table_5(Fifth_table))
-#    print(Fifth_table)
-#    print('\n Table 6\n')
-#    print(validate_table_6(Sixth_table))
-#    print(Sixth_table)
    
 
